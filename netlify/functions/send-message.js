727a79587b3d04f800573121c61e834ed7efd739
Sunday46fu <isunmonsa@gmail.com>
2026-05-08 09:12:28 +0700

    📝 อัปเดต/สร้างไฟล์ netlify/functions/send-message.js ผ่าน Mobile App

--- netlify/functions/send-message.js
+++ netlify/functions/send-message.js
@@ -4,12 +4,12 @@ exports.handler = async (event) => {
   if (event.httpMethod !== "POST") {
     return { statusCode: 405, body: "Method Not Allowed" };
   }
+
   try {
     const body = JSON.parse(event.body);
     const userId = body.userId;
     const message = body.message;
-    console.log("userId:", userId);
-    console.log("message:", message);
+
     const LINE_TOKEN = "G2tV047Ye/9jN50ooyrY6QhRCXip8f0/WzaV965OuzbxAXvRzLJQOyurIKf8wYdBWwB/zN43ATmJSU8ne+vj+RqMTb1iq0qy94ldu60t/Cl/1Pf4r54/0GZriA9ZRZ1RQpwuxwHX5mAUYqvbXKDMIwdB04t89/1O/w1cDnyilFU=";
 
     const result = await axios.post(
@@ -30,8 +30,10 @@ exports.handler = async (event) => {
     return { statusCode: 200, body: JSON.stringify({ status: "ok" }) };
 
   } catch (err) {
-    console.log("ERROR STATUS", err.response?.status);
-    console.log("ERROR DATA", JSON.stringify(err.response?.data));
-    return { statusCode: 500, body: JSON.stringify({ error: err.message }) };
+    console.log("ERROR", err.response?.status, JSON.stringify(err.response?.data));
+    return {
+      statusCode: 500,
+      body: JSON.stringify({ error: JSON.stringify(err.response?.data) })
+    };
   }
 };
