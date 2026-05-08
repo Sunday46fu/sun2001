eceb3fd18770261a8e74d9d7783ff354f49f36cd
Sunday46fu <isunmonsa@gmail.com>
2026-05-08 07:21:37 +0700

    📝 อัปเดต/สร้างไฟล์ netlify/functions/send-message.js ผ่าน Mobile App

--- netlify/functions/send-message.js
+++ netlify/functions/send-message.js
@@ -8,6 +8,8 @@ exports.handler = async (event) => {
     const body = JSON.parse(event.body);
     const userId = body.userId;
     const message = body.message;
+    console.log("userId:", userId);
+    console.log("message:", message);
     const LINE_TOKEN = "G2tV047Ye/9jN50ooyrY6QhRCXip8f0/WzaV965OuzbxAXvRzLJQOyurIKf8wYdBWwB/zN43ATmJSU8ne+vj+RqMTb1iq0qy94ldu60t/Cl/1Pf4r54/0GZriA9ZRZ1RQpwuxwHX5mAUYqvbXKDMIwdB04t89/1O/w1cDnyilFU=";
 
     const result = await axios.post(
